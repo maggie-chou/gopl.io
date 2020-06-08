edb355b229c461c03d563052c6c01968d2d3a468
maggie-chou <66622308+maggie-chou@users.noreply.github.com>
2020-06-08 19:32:05 +0800

    use a new line for the output result

--- ch1/dup1/main.go
+++ ch1/dup1/main.go
@@ -23,7 +23,7 @@ func main() {
 	// NOTE: ignoring potential errors from input.Err()
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+			fmt.Println("%d\t%s\n", n, line)
 		}
 	}
 }
